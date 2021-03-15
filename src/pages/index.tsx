3a893bcd86f609952efed8b56c088d0cf1a937d5
Lucas Leal <lucasvianil@gmail.com>
2021-03-14 23:21:12 -0300

    add work and contact sections structures and footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,19 @@ import {
   NameAndTitle,
   LinksBar,
   LinkSocialMedia,
+  About,
+  TitleBox,
+  AboutBox,
+  KnowledgeBox,
+  KnowledgeLine,
+  KnowledgeTitle,
+  KnowledgeItems,
+  Item,
+  Work,
+  Contact,
+  Footer,
+  // Copyright,
+  LinksFooterBox,
 } from '../styles/pages/Home';
 
 import { MontserratText, VidalokaText, RegularText } from '../components/Texts';
@@ -20,19 +33,19 @@ export default function Home() {
     <Container>
       <Greeting>
         <Header>
-          <ItemNav>
+          <ItemNav href="#about">
             <MontserratText size={18} weight="bold">
               about
             </MontserratText>
           </ItemNav>
 
-          <ItemNav>
+          <ItemNav  href="#work">
             <MontserratText size={18} weight="bold">
               work
             </MontserratText>
           </ItemNav>
 
-          <ItemNav>
+          <ItemNav  href="#contact">
             <MontserratText size={18} weight="bold">
               contact
             </MontserratText>
@@ -45,7 +58,7 @@ export default function Home() {
           </MontserratText>
 
           <Typer 
-            texts={["FullStack Javascript Developer.", "FrontEnd lover.", "Mobile enthusiast.","Passionate about sports and technology."]}
+            texts={["FullStack Javascript Developer.", "Front-end lover.", "Mobile enthusiast.","Passionate about sports and technology."]}
           />
         </NameAndTitle>
 
@@ -65,9 +78,133 @@ export default function Home() {
           <LinkSocialMedia target="_blank" href="https://medium.com/@lucasleal.dev">
             <FontAwesomeIcon icon={['fab', 'medium-m']} />
           </LinkSocialMedia>
-
         </LinksBar>
       </Greeting>
+
+      <About id="about">
+        <TitleBox>
+          <VidalokaText size={72}>about</VidalokaText>
+        </TitleBox>
+
+        <AboutBox>
+          Hi! I’m <strong>Lucas Leal</strong>, student of Computer Science on a great University in Brazil, UNESP.
+          <br /><br />
+          I’m a FullStack Javascript Developer, who <strong>loves</strong> Front-end and principally Mobile development, with 
+          strong knowledge in <strong>ReactJS</strong> and <strong>React Native</strong>.  I’ve always studying and practicing my skills in Back-end, 
+          with <strong>Node.js</strong>, to keep growing and be a better dev tomorrow than I was yesterday.
+        </AboutBox>
+      
+        <KnowledgeBox>
+          <KnowledgeLine>
+            <KnowledgeTitle>Languages</KnowledgeTitle>
+
+            <KnowledgeItems>
+              <Item><strong>JavaScript</strong></Item>
+              <span>●</span>
+
+              <Item><strong>TypeScript</strong></Item>
+              <span>●</span>
+
+              <Item>PHP</Item>
+              <span>●</span>
+
+              <Item>Java</Item>
+              <span>●</span>
+
+              <Item>C</Item>
+              <span>●</span>
+
+              <Item>Python</Item>
+            </KnowledgeItems>
+          </KnowledgeLine>
+        
+          <KnowledgeLine>
+            <KnowledgeTitle>Front-end</KnowledgeTitle>
+
+            <KnowledgeItems>
+              <Item><strong>HTML</strong></Item>
+              <span>●</span>
+
+              <Item><strong>CSS</strong></Item>
+              <span>●</span>
+
+              <Item><strong>React Native</strong></Item>
+              <span>●</span>
+
+              <Item>ReactJS</Item>
+              <span>●</span>
+
+              <Item>Responsive Websites</Item>
+              <span>●</span>
+
+              <Item><strong>Flexbox</strong></Item>
+              <span>●</span>
+
+              <Item>JQuery</Item>
+              <span>●</span>
+
+              <Item><strong>Bootstrap</strong></Item>
+            </KnowledgeItems>
+          </KnowledgeLine>
+        
+          <KnowledgeLine>
+            <KnowledgeTitle>Back-end</KnowledgeTitle>
+
+            <KnowledgeItems>
+              <Item><strong>Node.js</strong></Item>
+              <span>●</span>
+
+              <Item><strong>MySQL</strong></Item>
+              <span>●</span>
+
+              <Item>Docker</Item>
+              <span>●</span>
+
+              <Item>Postgres</Item>
+              <span>●</span>
+
+              <Item>MongoDB</Item>
+              <span>●</span>
+
+              <Item>Redis</Item>
+            </KnowledgeItems>
+          </KnowledgeLine>
+        </KnowledgeBox>
+      </About>
+
+      <Work id="work">
+        <TitleBox>
+          <VidalokaText size={72}>work</VidalokaText>
+        </TitleBox>
+      </Work>
+
+      <Contact id="contact">
+        <TitleBox>
+          <VidalokaText size={72}>contact</VidalokaText>
+        </TitleBox>
+      </Contact>
+    
+      <Footer>
+        <RegularText weight="bold" color="#FFFFFF">Copyright © Lucas Leal 2021</RegularText>
+
+        <LinksFooterBox>
+          <LinkSocialMedia target="_blank" href="https://www.linkedin.com/in/lucas-viani-leal/">
+            <FontAwesomeIcon icon={['fab', 'linkedin-in']} />
+          </LinkSocialMedia>
+
+          <LinkSocialMedia target="_blank" href="https://github.com/lucasvleal">
+            <FontAwesomeIcon icon={['fab', 'github']} />
+          </LinkSocialMedia>
+
+          <LinkSocialMedia target="_blank" href="https://twitter.com/luquinha_txt">
+            <FontAwesomeIcon icon={['fab', 'twitter']} />
+          </LinkSocialMedia>
+
+          <LinkSocialMedia target="_blank" href="https://medium.com/@lucasleal.dev">
+            <FontAwesomeIcon icon={['fab', 'medium-m']} />
+          </LinkSocialMedia>
+        </LinksFooterBox>
+      </Footer>
     </Container>
   )
 }
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -4,12 +4,12 @@ import { FlexContainer, FlexContainerColumn } from '../GeneralComponents';
 
 export const Container = styled(FlexContainerColumn)`
     background: ${({ theme }) => theme.color.white};
-    padding: 0 ${({ theme }) => theme.size.padding.horizontalContainer};
 `;
 
 // <GREETING>
 export const Greeting = styled.div`
     height: 100vh;
+    padding: 0 ${({ theme }) => theme.size.padding.horizontalContainer};
 
     /* background: red; */
 `;
@@ -37,6 +37,7 @@ export const ItemNav = styled.a`
     & > span {
         border-bottom: 2px solid transparent;
         cursor: pointer;
+        color: black;
     }
 
     & > span:hover {
@@ -62,9 +63,7 @@ export const LinksBar = styled(FlexContainer)`
     height: 10vh;
 
     align-items: center;
-    justify-content: center;
-
-    
+    justify-content: center;    
 
     /* background: tomato; */
 `;
@@ -93,8 +92,123 @@ export const LinkSocialMedia = styled.a`
         }
     }
 `;
-
-
-
-
 // </GREETING>
+
+// <ABOUT>
+export const About = styled.div`
+    height: 100vh;
+    padding: 0 ${({ theme }) => theme.size.padding.horizontalContainer};
+
+    background-color: ${({ theme }) => theme.color.backgroundColored};
+`;
+
+export const TitleBox = styled(FlexContainer)`
+    justify-content: center;
+    align-items: center;
+
+    height: 20vh;
+
+    /* background-color: red; */
+`;
+
+export const AboutBox = styled.div`
+    line-height: 40px;
+    font-size: ${({ theme }) => theme.size.font.normal};
+
+    /* background-color: red; */
+`;
+
+export const KnowledgeBox = styled.div`
+    padding-top: 40px;
+`;
+
+export const KnowledgeLine = styled(FlexContainer)`
+    align-items: center;
+
+    margin-top: 20px;
+`;
+
+export const KnowledgeTitle = styled.div`
+    /* display: flex; */
+    /* justify-content: center; */
+    /* align-items: center; */
+
+    padding: 1vh 0;
+    width: 12vw;
+    margin-right: 1vw;
+    border: 2px solid black;
+    border-radius: 5px;
+
+    text-align: center;
+    font-weight: bold;
+`;
+
+export const KnowledgeItems = styled(FlexContainer)`
+    span { 
+        display: flex;
+        align-items: center;
+
+        font-size: 4px;
+    }
+`;
+
+export const Item = styled.div`
+    margin: 0 8px;
+`;
+// </ABOUT>
+
+// <WORK>
+export const Work = styled.div`
+    height: 100vh;
+    padding: 0 ${({ theme }) => theme.size.padding.horizontalContainer};
+
+    background-color: ${({ theme }) => theme.color.white};
+`;
+// </WORK>
+
+// <CONTACT>
+export const Contact = styled.div`
+    height: 100vh;
+    padding: 0 ${({ theme }) => theme.size.padding.horizontalContainer};
+
+    background-color: ${({ theme }) => theme.color.backgroundColored};
+`;
+// </CONTACT>
+
+// <FOOTER>
+export const Footer = styled(FlexContainer)`
+    align-items: center;
+    justify-content: space-between;
+    
+    height: 20vh;
+    padding: 0 ${({ theme }) => theme.size.padding.horizontalContainer};
+
+    background-color: ${({ theme }) => theme.color.black};
+`;
+
+export const LinksFooterBox = styled(FlexContainer)`
+    &  svg {
+        cursor: pointer;
+        transition: 0.2s;
+
+        color: white;
+
+        margin: 0 20px;
+        font-size: 32px;
+    }
+
+    &  svg:hover {
+       color: ${({ theme }) => theme.color.mediumGray};
+       transition: 0.2s;
+    }
+
+    @media (max-width: 768px) {
+        & svg {
+            font-size: 24px;
+        }
+    }
+`;
+// </FOOTER>
+
+
+
